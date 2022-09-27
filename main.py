974e5f89ed17545c7864733c12343954f248b7d7
Joao Frankmann <jfrankmann@Joaos-MacBook-Pro.local>
2022-09-27 13:19:41 +0100

    finished user input cli

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from operator import itemgetter
 
 dataset = './data/dataset.csv'
 
+
 def csv_to_list(dataset):
     """Converts CSV file to List"""
     with open(dataset, 'r') as read_obj:
@@ -13,24 +14,26 @@ def csv_to_list(dataset):
         return raw_csv_list
 
 def current_rent():
-    """Produces a list sorted by “Current Rent” in ascending order"""
+    """Ascending list sorted by “Current Rent”"""
     csv_list = csv_to_list(dataset)
     header = csv_list.pop(0)
+    rent_index = header.index('Current Rent')
     for sublist in csv_list:
-        sublist[-1] = float(sublist[-1])
+        sublist[rent_index] = float(sublist[rent_index])
     sorted_list = sorted(csv_list, key=itemgetter(-1))
     for sublist in sorted_list:
-        sublist[-1] = str(sublist[-1])
+        sublist[rent_index] = str(sublist[rent_index])
     sorted_list.insert(0,header)
-    return sorted_list 
+    return sorted_list
 
 def five_first_items():
-    """Returns a list with the five first items of the “Current Rent” list"""
+    """List five first items of the “Current Rent”"""
     sorted_list = current_rent()
     return sorted_list[0:6]
 
+
 def lease_years_list():
-    """Returns a list with the Tenants with 25 years lease """
+    """List tenants with 25 years lease"""
     csv_list = csv_to_list(dataset)
     header = csv_list.pop(0)
     lease_years_index = header.index('Lease Years')
@@ -41,17 +44,19 @@ def lease_years_list():
     lease_years_list.insert(0,header)
     return lease_years_list
 
+
 def total_rent():
-    """Returns the total rent for the list of tenanants with 25 years lease"""
+    """Returns the total rent with 25 years lease"""
     lease_25_years = lease_years_list()[1:]
     total =[]
     for sublist in lease_25_years:
         sublist[-1] = float(sublist[-1])
         total.append(sublist[-1])
     return sum(total)
-    
+
+
 def dict_tenant_name_and_masts():
-    """Returns the Tenants Names and their amount of Masts"""
+    """Dict: Tenants Names and amount of Masts"""
     csv_list = csv_to_list(dataset)
     header = csv_list.pop(0)
     tenant_index = header.index('Tenant Name')
@@ -83,10 +88,9 @@ def dict_tenant_name_and_masts():
     final_results = dict(zip(keys_list, values_list))
     return final_results
 
-    
+  
 def lease_start_date():
-    """Returns a list with the Tenants with 
-    “Lease Start Date” between 1st June 1999 and 31st August 2007"""
+    """Lease Start Date btw 1/6/99-31/08/07"""
     csv_list = csv_to_list(dataset)
     header = csv_list.pop(0)
     lease_start_date_index = header.index('Lease Start Date')
@@ -103,4 +107,42 @@ def lease_start_date():
     lease_start_date.insert(0,header)
     return lease_start_date
 
-print(lease_start_date())
+
+if __name__ == '__main__':
+    choice = ''
+    while choice != '0':
+        print("\n[1] 'Current Rent' in ascending order")
+        print("[2] Obtain first 5 items of 'Current Rent'")
+        print("[3] Mast data with 'Lease Years' = 25 years")
+        print("[4] Total rent for all items in mast data = 25 years")
+        print("[5] Tenant name and count of masts for each tenant")
+        print("[6] Rentals with Lease Start Date btw 1/6/99-31/08/07")
+        print("[all] Show all results above")
+        print("[0] Exit")
+        
+        choice = input("\nChoose one option: ")
+        
+        if choice == '1':
+            print(current_rent())
+        elif choice == '2':
+            print(five_first_items())
+        elif choice == '3':
+            print(lease_years_list())
+        elif choice == '4':
+            print(total_rent())
+        elif choice == '5':
+            print(dict_tenant_name_and_masts())
+        elif choice == '6':
+            print(lease_start_date())
+        elif choice == 'all':
+            print(current_rent())
+            print(five_first_items())
+            print(lease_years_list())
+            print(total_rent())
+            print(dict_tenant_name_and_masts())
+            print(lease_start_date())
+        elif choice == '0':
+            pass
+        else:
+            print("\nWrong option, please try again.\n")
+    print("Bye.")
